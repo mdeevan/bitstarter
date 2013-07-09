90cdce0e8f8a59e5af992848591f559a199d9f2a
Ubuntu <ubuntu@ip-172-31-4-41.us-west-2.compute.internal>
2013-07-09 23:53:40 +0000

    option URL added

--- grader.js
+++ grader.js
@@ -26,7 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
- 
+var rest = require('restler');
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -55,6 +56,33 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkURL = function(URL, checksfile) {
+
+	rest.get(URL).on('complete', function(result) {
+	    var out = {};
+		
+		if (result instanceof Error) {
+
+			out = "Error reading the URL" + result;
+			
+  			//console.log("Error reading the URL %s. Exiting.", result);
+        	//process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+
+  		} else {
+    		$ = result;
+
+		    var checks = loadChecks(checksfile).sort();
+		    for(var ii in checks) {
+		        var present = $(checks[ii]).length > 0;
+		        out[checks[ii]] = present;
+		    }	
+  		}
+	});
+	
+    return out;
+	
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,8 +93,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+        
+    if (program.url)    
+	    var checkJson = checkURL(program.url, program.checks);
+	else
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
